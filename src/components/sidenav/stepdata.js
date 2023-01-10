c46af5f3864c8759a658beeda164c16c49954ec4
Henok Belachew <henokbelachewstu@gmail.com>
2023-01-11 00:11:17 +0300

    step nav fixed

--- src/components/sidenav/stepdata.js
+++ src/components/sidenav/stepdata.js
@@ -1,21 +1,21 @@
 const stepData = [
     {
-        num: "1",
+        num: 1,
         step: "STEP 1",
         title: "YOUR INFO"
     },
     {
-        num: "2",
+        num: 2,
         step: "STEP 2",
         title: "SELECT PLAN"
     },
     {
-        num: "3",
+        num: 3,
         step: "STEP 3",
         title: "ADD-ONS"
     },
     {
-        num: "4",
+        num: 4,
         step: "STEP 4",
         title: "SUMMARY"
     },
